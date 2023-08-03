8acb8146f12c3bf0987c4668cb4019ec3395109c
Arsenic <airajena07@gmail.com>
2023-08-03 12:04:07 +0530

    Completed

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,5 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 export function absoluteUrl(path: string) {
-  return `${process.env.NEXT_PUBLIC_BASE_URL}${path}`
+  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
 }
